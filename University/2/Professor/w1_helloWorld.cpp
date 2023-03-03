d281756cbb70121e9777e7713a804165f016f97b
ireongi <ireongi@gmail.com>
2023-03-03 17:24:52 +0900

    ,

---
+++ University/2/Professor/w1_helloWorld.cpp
@@ -0,0 +1,17 @@
+// comments
+// Ctrl+k+c, Ctrl+k+u
+
+//#include <iostream>  // input output stream
+//using namespace std;  // std stands for Standard namespace
+//
+//int main() {  // program entry point
+//	//std::cout << "Hello World!" << std::endl;
+//	//cout << "Hello World!" << endl;
+//	//cout << "Hello World!" << "\n";
+//	//cout << "Hello World!\n"; // cout stands for Character OUTput stream object
+//	string name;
+//	cin >> name; // cin stands for Character INput stream object
+//	cout << "Hello " << name <<"\n";
+//
+//	return 0;
+//}
